bc7e25d21ebe7c181902307d27a69b2153cabfc2
Gustavo Mendes <gusttavo.me@outlook.com>
2024-02-12 16:17:55 -0300

    alioth: parts: Rework thermal profiles impl
    Co-authored-by: ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ie1f933e620b6e30fc717c295656099a8b798703f
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -20,8 +20,8 @@ import android.app.ActivityManager;
 import android.app.ActivityTaskManager;
 import android.app.ActivityTaskManager.RootTaskInfo;
 import android.app.IActivityTaskManager;
-import android.app.TaskStackListener;
 import android.app.Service;
+import android.app.TaskStackListener;
 import android.app.TaskStackListener;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -30,42 +30,80 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.RemoteException;
-import android.provider.Settings;
 import android.util.Log;
-
-import java.util.Arrays;
 
 public class ThermalService extends Service {
 
     private static final String TAG = "ThermalService";
     private static final boolean DEBUG = false;
-    private static final String SETTINGS_GAME_LIST = "gamespace_game_list";
 
-    private String mPreviousApp;
+    private boolean mScreenOn = true;
+    private String mCurrentApp = "";
     private ThermalUtils mThermalUtils;
 
     private IActivityTaskManager mActivityTaskManager;
 
+    private final TaskStackListener mTaskListener = new TaskStackListener() {
+        @Override
+        public void onTaskStackChanged() {
+            try {
+                final RootTaskInfo info = mActivityTaskManager.getFocusedRootTaskInfo();
+                if (info == null || info.topActivity == null) {
+                    return;
+                }
+
+                String foregroundApp = info.topActivity.getPackageName();
+                if (!foregroundApp.equals(mCurrentApp)) {
+                    mCurrentApp = foregroundApp;
+                    setThermalProfile();
+                }
+            } catch (Exception e) {
+            }
+        }
+    };
+
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            mPreviousApp = "";
-            mThermalUtils.setDefaultThermalProfile();
+            switch (intent.getAction()) {
+                case Intent.ACTION_SCREEN_OFF:
+                    mScreenOn = false;
+                    setThermalProfile();
+                    break;
+                case Intent.ACTION_SCREEN_ON:
+                    mScreenOn = true;
+                    setThermalProfile();
+                    break;
+            }
         }
     };
 
     @Override
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
+        mThermalUtils = new ThermalUtils(this);
         try {
             mActivityTaskManager = ActivityTaskManager.getService();
             mActivityTaskManager.registerTaskStackListener(mTaskListener);
         } catch (RemoteException e) {
             // Do nothing
         }
-        mThermalUtils = new ThermalUtils(this);
         registerReceiver();
         super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        if (DEBUG) Log.d(TAG, "Destroying service");
+        unregisterReceiver();
+        try {
+            ActivityTaskManager.getService().unregisterTaskStackListener(mTaskListener);
+        } catch (RemoteException e) {
+            // Do nothing
+        }
+        mThermalUtils.setDefaultThermalProfile();
+        mThermalUtils = null;
+        super.onDestroy();
     }
 
     @Override
@@ -86,42 +124,15 @@ public class ThermalService extends Service {
         this.registerReceiver(mIntentReceiver, filter);
     }
 
-    private boolean isListedOnGameSpace(String packageName) {
-        String[] gameList = Settings.System.getString(getContentResolver(),
-                SETTINGS_GAME_LIST).split(";");
-        if (packageName == null || gameList.length == 0) {
-            return false;
+    private void setThermalProfile() {
+        if (mScreenOn) {
+            mThermalUtils.setThermalProfile(mCurrentApp);
+        } else {
+            mThermalUtils.setDefaultThermalProfile();
         }
-
-        return Arrays.stream(gameList).map(data -> {
-            String[] userGame = data.split("=");
-            return userGame.length == 2 ? userGame[0] : data;
-        }).anyMatch(it -> it.equals(packageName));
     }
 
-    private boolean isConfigured(String packageName) {
-        return mThermalUtils.getStateForPackage(packageName) != ThermalUtils.STATE_DEFAULT;
+    private void unregisterReceiver() {
+        this.unregisterReceiver(mIntentReceiver);
     }
-
-    private final TaskStackListener mTaskListener = new TaskStackListener() {
-        @Override
-        public void onTaskStackChanged() {
-            try {
-                final RootTaskInfo info = mActivityTaskManager.getFocusedRootTaskInfo();
-                if (info == null || info.topActivity == null) {
-                    return;
-                }
-
-                String foregroundApp = info.topActivity.getPackageName();
-                if (!foregroundApp.equals(mPreviousApp)) {
-                    if (!isConfigured(foregroundApp) && isListedOnGameSpace(foregroundApp)) {
-                        mThermalUtils.setThermalProfileForce(ThermalUtils.STATE_GAMING);
-                    } else {
-                        mThermalUtils.setThermalProfile(foregroundApp);
-                    }
-                    mPreviousApp = foregroundApp;
-                }
-            } catch (Exception e) {}
-        }
-    };
 }
