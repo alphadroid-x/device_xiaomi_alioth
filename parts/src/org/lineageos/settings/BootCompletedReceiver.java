559c1f6091ba47510a5b6b2d5b03e7deac02163b
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2024-02-12 15:50:29 -0300

    alioth: parts: Start services as early as possible on bootup BOOT_COMPLETED intent is broadcast only after the device is unlocked and user data (encrypted storage) is available. Use LOCKED_BOOT_COMPLETED intent for services that don't require accessing data.
    While we're at it, target latest SDK and uprev the package.
    
    Change-Id: I8431d949c515941360c3258efd54fc2fc785f6df
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,8 +32,24 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        Log.i(TAG, "Received intent: " + intent.getAction());
 
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        switch (intent.getAction()) {
+            case Intent.ACTION_LOCKED_BOOT_COMPLETED:
+                onLockedBootCompleted(context);
+                break;
+            case Intent.ACTION_BOOT_COMPLETED:
+                onBootCompleted(context);
+                break;
+        }
+    }
+
+    private static void onLockedBootCompleted(Context context) {
+        // Services that don't require reading from data.
+    }
+    
+    private static void onBootCompleted(Context context) {
+        // Data is now accessible (user has just unlocked).
         ThermalUtils.initialize(context);
     }
 }
