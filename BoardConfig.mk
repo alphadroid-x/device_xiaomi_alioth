c5fe533b5b51053521ce52018410f067ab993fdc
Gustavo Mendes <gusttavo.me@outlook.com>
2024-03-07 14:01:03 -0300

    alioth: Remove duplicate slash in vintft section
    Change-Id: Idbaff0110d5c5978d2f37b06b329c26214ff65b2
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,10 +92,10 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/device_framework_matrix.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)//configs/vintf/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)//configs/vintf/manifest.xml
+DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 ODM_MANIFEST_SKUS += alioth
-ODM_MANIFEST_ALIOTH_FILES := $(DEVICE_PATH)//configs/vintf/manifest_nfc.xml
+ODM_MANIFEST_ALIOTH_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_alioth
