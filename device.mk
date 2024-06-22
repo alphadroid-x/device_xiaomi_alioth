41b7e1642c0ce054b8924c805d3a100bd54510e6
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-22 19:24:15 +0200

    alioth: Fix camera library rule location
    Change-Id: I24b30a7e187273fce2e23847debe5bff473bfe23

--- device.mk
+++ device.mk
@@ -186,7 +186,6 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
 
@@ -194,6 +193,7 @@ PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++_vendor \
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
